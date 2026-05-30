15fcf92a4e9a92cef02a3e1b609858eda98e8fd6
Ilse Guerra <ilsecervantes177@gmail.com>
2026-05-30 02:04:41 -0600

    Librerias

--- src/app.js
+++ src/app.js
@@ -17,6 +17,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
+app.use(express.json()); 
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) =>{
     res.json({
