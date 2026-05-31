f000586babc74f5d866e1d8fa0fc37f8b90212b4
Ilse Guerra <ilsecervantes177@gmail.com>
2026-05-31 17:27:15 -0600

    cambio en routers matsuc

--- src/routers/material_sucursal.routers.js
+++ src/routers/material_sucursal.routers.js
@@ -18,10 +18,4 @@ router.get('/', verificarToken, material_sucursalController.obtenerMateriales);
 // Eliminar uno (usa :id porque necesitamos saber cuál borrar)
 router.delete('/:id', verificarToken, material_sucursalController.eliminarMaterial);
 
-/**
- * @openapi
- * description: Lista de materiales obtenida exitosamente
- */
-router.get('/', materialController.getAll);
-
 module.exports = router;
