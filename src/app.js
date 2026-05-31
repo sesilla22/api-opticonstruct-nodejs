6c844f3ae401e72ff5698159018f19cec2fa4f9c
Ilse Guerra <ilsecervantes177@gmail.com>
2026-05-31 16:07:08 -0600

    Limpieza y arreglos finales

--- src/app.js
+++ src/app.js
@@ -14,8 +14,10 @@ const authRouters = require('./routers/auth.routers'); //
 
 const app = express();
 
+
 app.use(cors());
 
+//Librerias que agregué para swagger y vistas
 app.use(express.json());
 app.use(express.static('public'));
 
